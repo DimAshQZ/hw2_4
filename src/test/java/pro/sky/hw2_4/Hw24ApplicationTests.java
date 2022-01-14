a3bc5699587db5b1f6f1d09045d575a0b077a8d2
DimAshQZ <53437421+DimAshQZ@users.noreply.github.com>
2022-01-14 11:29:04 +0600

    Initial commit

---
+++ src/test/java/pro/sky/hw2_4/Hw24ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw2_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
